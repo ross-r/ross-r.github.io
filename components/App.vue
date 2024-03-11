28f3653cc6f8de1797b77bed5834f91896670582
ross-r <80952041+ross-r@users.noreply.github.com>
2024-03-11 16:03:04 +0800

    Added tailwindcss, created pages/ and components/ and added main App and NavBar components for now

---
+++ components/App.vue
@@ -0,0 +1,9 @@
+<template>
+  
+  <main class="select-none scroll-smooth leading-normal tracking-normal text-stone-100 min-h-screen p-6 bg-neutral-900">
+    
+    <NavBar />
+  
+  </main>
+  
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    data() {
+        return {
+            entries: [
+                {
+                    label: "Blog",
+                    href: "/blog"
+                },
+                {
+                    label: "Work",
+                    href: "/work"
+                },
+                {
+                    label: "Contact",
+                    href: "/contact"
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="w-full container mx-auto">
+        <div class="w-full flex items-center justify-between">
+            <a href="/" class="flex items-center no-underline hover:no-underline font-bold lg:text-4xl">
+                <span>MyLogo</span>
+            </a>
+
+            <div class="flex justify-end content-center uppercase">
+                <div class="font-semibold cursor-pointer inline-block text-blue-100 no-underline hover:text-blue-400 text-center h-auto transform duration-300 ease-in-out" v-for="entry in entries">
+                    <a class="p-4" :href="`${entry.href}`">{{ entry.label }}</a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="px-8 pt-12 mx-auto w-full h-0.5">
+            <span class="w-full h-0.5 block bg-neutral-800"></span>
+        </div>
+    </div>
+
+</template>
