7ed1a26133ce410048ab197cd70e8ac595c7f06c
ross-r <80952041+ross-r@users.noreply.github.com>
2024-03-13 14:28:50 +0800

    Fixed a typo

--- components/Home.vue
+++ components/Home.vue
@@ -12,7 +12,7 @@
             <div class="max-w-full min-w-full">
               <h1 class="text-4xl font-normal dark:text-white">Hey, I'm Ross.</h1>
               <p class="py-6 mb-0 pb-0">I'm a software developer from Perth, Australia.</p>
-              <p class="mt-0 pt-0 pb-4">I'm a versatile developer who is compotent in many languages.</p>
+              <p class="mt-0 pt-0 pb-4">I'm a versatile developer who is competent in many languages.</p>
               <a class="btn btn-primary" href="/projects">Projects</a>
             </div>
           </div>
