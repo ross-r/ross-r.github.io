bb8e2e69eaefb09c906997c3fb387bc64f03e9cd
ross-r <80952041+ross-r@users.noreply.github.com>
2024-04-02 02:21:26 +0800

    Fixed layout on mobile devices

--- components/BaseLayout.vue
+++ components/BaseLayout.vue
@@ -4,7 +4,7 @@
     <NavBar />
 
     <main
-      class="select-none scroll-smooth leading-normal tracking-normal min-h-screen pt-8 mx-auto px-8 w-fit md:px-0 md:w-3/4 2xl:w-2/4">
+      class="select-none scroll-smooth leading-normal tracking-normal min-h-screen mx-4 md:pt-8 md:mx-auto md:px-0 md:w-3/4 2xl:w-2/4">
 
       <slot name="main"></slot>
 
